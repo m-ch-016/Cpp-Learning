d9b9d8b1c6cf60c86226fb698ff12d237e1d98d4
Muhammed A. Choudhary <muhammed.choudhary@student.manchester.ac.uk>
2025-05-31 18:37:42 +0100

    Created a fibonacci function

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 
 using namespace std;
 
+int fib(int number) {
+    if (number == 1 || number == 0 ) {
+        return number;
+    }
+    
+    return fib(number-1) + fib(number - 2);
+}
+
 int main()
 {
-    std::cout << "Hello World!";
-    
+    int num1;
+
+    std::cout << "Enter a number to find the fib of: ";
+    std::cin >> num1;
+
+    std::cout << "Fibonacci of " << num1 << " is " << fib(num1);
+
 }
